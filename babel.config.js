f07722817ea64b00c69b66d722c0b264b7244d2b
viniszymanowski <viniszymanowski@gmail.com>
2026-02-12 18:44:34 -0300

    uuj

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
-
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
-    plugins: [
-      "nativewind/babel",
-      // SEMPRE por último:
-      "react-native-reanimated/plugin",
-    ],
+    plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -3,11 +3,4 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// necessário para css no RN / web
-config.transformer.minifierConfig = {
-  ...config.transformer.minifierConfig,
-  keep_classnames: true,
-  keep_fnames: true,
-};
-
 module.exports = withNativeWind(config, { input: "./global.css" });
