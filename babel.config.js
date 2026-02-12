2e973f8ba198cc0527a5f2443586037a8f097496
viniszymanowski <viniszymanowski@gmail.com>
2026-02-12 18:24:30 -0300

    reggt

--- babel.config.js
+++ babel.config.js
@@ -5,9 +5,7 @@ module.exports = function (api) {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
     plugins: [
       "nativewind/babel",
-      // se você REALMENTE usa react-native-worklets:
-      "react-native-worklets/plugin",
-      // reanimated SEMPRE por último
+      // SEMPRE por último:
       "react-native-reanimated/plugin",
     ],
   };
