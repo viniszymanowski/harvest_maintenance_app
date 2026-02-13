7955907843b274dd09faa0380579de19ced525d4
viniszymanowski <viniszymanowski@gmail.com>
2026-02-13 20:50:45 -0300

    dd

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,11 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+
+// expo-sqlite (web) uses a .wasm asset; ensure Metro treats it as an asset on web.
+config.resolver.assetExts = config.resolver.assetExts || [];
+if (!config.resolver.assetExts.includes("wasm")) {
+  config.resolver.assetExts.push("wasm");
+}
+
 module.exports = withNativeWind(config, { input: "./global.css" });
